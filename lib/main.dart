448e7818d9444a62ff728b647e6c952f34546614
oalshokri <janataanfor@gmail.com>
2023-08-15 13:27:04 +0300

    provider string

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'top secret data';
+  final String data = 'test';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(data),
+    return Provider<String>(
+      create: (BuildContext context) => data,
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(data),
+          ),
+          body: Level1(),
         ),
-        body: Level1(data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  String data;
-  Level1(this.data);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  String data;
-  Level2(this.data);
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Level3(data),
+        Level3(),
       ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  String data;
-  Level3(this.data);
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return Text(Provider.of<String>(context));
   }
 }
